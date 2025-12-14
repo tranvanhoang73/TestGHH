d6c538632b537f421d61b85d4b16da54a88774a1
Tran Van Hoang <hoangtvh73@gmail.com>
2025-12-14 09:50:22 +0700

    1 commit

---
+++ src/main/java/com/example/testghh/TestGHH.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.testghh;
+
+/**
+ *
+ * @author Admin
+ */
+public class TestGHH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
